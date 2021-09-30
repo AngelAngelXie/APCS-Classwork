28037ae980168f3e4656c98539bc32bf1819c65e
JiaXin Xie <jxie21@stuy.edu>
2021-09-30 17:53:04 -0400

    initial problem

---
+++ ArrayTest.java
@@ -0,0 +1,11 @@
+public class ArrayTest {
+    public static void main(String[] args){
+        //int [] anArray = ???
+        //Write the loop to initialize it
+        System.out.println("part 1:");
+        //print the array variable
+        System.out.println("part 2:");
+        // write a loop to print it out(on one line)
+
+    }
+}
