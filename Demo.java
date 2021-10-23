a09f5e0704f154e23d1f3f42e7058c0dc320780e
Angel Xie <jxie21@stuy.edu>
2021-10-23 17:15:49 -0400

    cannot find symbol?

--- Demo.java
+++ Demo.java
@@ -1,14 +1,17 @@
 public class Demo{
   /* more methods can go here*/
 
+
   public static void main(String[]args){
     Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
 
-    System.out.println(  distance(P1,P2)); //call1
-    System.out.println(  Point.distance(P1,P2)); //call 2
-    System.out.println(  P1.distanceTo(P2)); //call 3
-    //This one is IMPLIED strongly. You cannot tell by the calling style alone,
+    distance(P1, P2);
+    P1.distanceTo(P2);
+    System.out.println(distance(P1,P2)); //call1 static
+    System.out.println(Point.distance(P1,P2)); //call 2 static
+    System.out.println(P1.distanceTo(P2)); //call 3 non-static
+    // //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
   }
  }
--- Point.java
+++ Point.java
@@ -1,5 +1,8 @@
 public class Point{
+// field
   private double x,y;
+
+//constructot
   public Point(double X, double Y){
     x=X;
     y=Y;
@@ -8,10 +11,24 @@ public class Point{
     x= p.x;
     y= p.y;
   }
+
+  //methods
   public double getX(){
     return x;
   }
   public double getY(){
     return y;
   }
+
+
+  public double distanceTo(Point A){
+    double result = Math.sqrt((x - A.x) * (x - A.x) +(y - A.y) * (y - A.y));
+    return result;
+  }
+
+  public static double distance(Point A, Point B){
+    double result = Math.sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
+    return result;
+  }
+
 }
