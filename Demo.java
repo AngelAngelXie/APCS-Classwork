7d67215a73025f9edf64f47997a8959d0a2694d8
Angel Xie <jxie21@stuy.edu>
2021-10-25 18:50:05 -0400

    fixed typo! Demo now runs!

--- Demo.java
+++ Demo.java
@@ -2,12 +2,15 @@ public class Demo{
   /* more methods can go here*/
 
 
+  public static double distance(Point A, Point B){
+    double result = Math.sqrt((A.getX() - B.getX()) * (A.getX() - B.getX()) + (A.getY() - B.getY()) * (A.getY() - B.getY()));
+    return result;
+  }
+
   public static void main(String[]args){
     Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
 
-    distance(P1, P2);
-    P1.distanceTo(P2);
     System.out.println(distance(P1,P2)); //call1 static
     System.out.println(Point.distance(P1,P2)); //call 2 static
     System.out.println(P1.distanceTo(P2)); //call 3 non-static
