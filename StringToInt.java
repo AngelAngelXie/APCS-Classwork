6d304f5634fd1f7066551e5910b2d94866d07a82
Angel Xie <jxie21@stuy.edu>
2021-12-23 08:58:03 -0500

    added heper + main function

--- StringToInt.java
+++ StringToInt.java
@@ -1,7 +1,16 @@
 public class StringToInt{
+  public static int valueOfDigit(char c){
+    int result = 0;
+    result = c - 48;
+    return result;
+  }
+
   public static int stringToInt(String s){
       //return the value of Integer.parseInt(s) but do it yourself!
       //Do not use any built in parse methods.
-      
+      int result = 0;
+      for(int i = 0; i<s.length(); i++){
+        result = valueOfDigit(s[i]) * 10 * s.length()-(i+1);
+      } return result;
   }
 }
